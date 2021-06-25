5be328e951664c1cc19096041b4f9f1f3fb7dab9
Vivi Cowan <vivicare@gmail.com>
2021-06-24 17:43:30 -0700

    changed date format

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var currentDay = moment();
-$("#currentDay").text(currentDay.format('MMMM Do, YYYY'));
+$("#currentDay").text(currentDay.format('dddd, MMMM Do'));
 
 var hour = moment().format('HH');
 
@@ -38,10 +38,17 @@ $(".saveBtn").on("click", function(event){
 	var userInfo = $(this).siblings().eq(1).val();
 	info = $(this).parent().attr("info");
 	localStorage.setItem(info, userInfo);
-	alert(localStorage.getItem(info) + " is saved!");
 });
 
-
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 
